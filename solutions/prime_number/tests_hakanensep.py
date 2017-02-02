05ee1517e2eb50e4fa43e7399311b41d503a5b74
Hakan ENSEP <hakan.ensep@metglobal.com>
2017-02-02 13:52:18 +0300

    unit test testing

---
+++ solutions/prime_number/tests_hakanensep.py
@@ -0,0 +1,14 @@
+from prime import is_prime
+
+def test_good():
+	assert is_prime(3) is True
+def test_one():
+	assert is_prime(1) is False
+def test_string():
+	assert is_prime('1') is False
+def test_some():
+	assert is_prime('') is False
+def test_negative():
+	assert is_prime(-1) is False
+def test_string():
+	assert is_prime("test") is False
